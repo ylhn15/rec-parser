a64d1680b8918d1073f4a91a8e8c87d329640974
Yannick Lehnhausen <yannick@lehnhausen.dev>
2024-10-04 19:18:25 +0200

    add get-by-id

--- src/rec_parser/core.clj
+++ src/rec_parser/core.clj
@@ -8,12 +8,37 @@
   (filter #(not (str/starts-with? % "%"))
           (str/split-lines (slurp "./house.rec"))))
 
+(type "Test")
+
 (defn pretty-print [output]
-  (dorun (map #(println %) output)))
+  (if (= (type output) java.lang.String)
+    (println output)
+    (dorun (map #(println %) output))))
+
+(defn split-at-empty-line [input]
+  (->> input
+       (partition-by #(= % ""))
+       (filter #(seq %))))
+
+(defn remove-empty-entries [nested]
+  (->> nested
+       (mapv #(vec (remove empty? %)))
+       (filterv seq)))
+
+(defn get-by-id [input  id]
+  (-> input
+      rest
+      split-at-empty-line
+      remove-empty-entries
+      (nth id "Not found")))
+
+(get-by-id data 2)
 
 (defn get-keys [input]
-  (pretty-print
-   (into [] (distinct (map #(subs % 0 (str/index-of % ":")) (filter #(not (str/blank? %)) input))))))
+  (->> input
+       (filter #(not (str/blank? %)))
+       (map #(subs % 0 (str/index-of % ":")))
+       distinct))
 
 (defn get-by-key [input key]
   (->> input
@@ -38,6 +63,12 @@
   {:spec
    {:get-keys {:desc "Print all keys in rec-file"
                :alias :gk}
+    :get {:coerce :string
+          :desc "Get all values with key"
+          :alias :g}
+    :get-by-id {:coerce :int
+                :desc "Get values by id"
+                :alias :id}
     :sum {:coerce :string
           :desc "Sum up the values with key"
           :alias :s}}
@@ -57,8 +88,10 @@
   (let [opts (cli/parse-opts args cli-spec)]
     (if (or (:help opts) (:h opts))
       (println (show-help cli-spec))
-      (cond (contains? opts :get-keys) (get-keys data)
-            (contains? opts :sum) (println (sum data (get opts :sum)))
+      (cond (contains? opts :get-keys) (pretty-print  (get-keys data))
+            (contains? opts :sum) (pretty-print (sum data (get opts :sum)))
+            (contains? opts :get) (pretty-print (get-by-key data (get opts :get)))
+            (contains? opts :get-by-id) (pretty-print (get-by-id data (get opts :get-by-id)))
             :else
             (println "Here are your cli args!:" opts)))))
 
